73fc929f28be728900e46bb56c12ccaeca5f0f69
Shreya Kumari <157014499+shreya-arya00@users.noreply.github.com>
2024-04-05 11:35:43 +0530

    Create Product.java

---
+++ Domain Layer/Product.java
@@ -0,0 +1,31 @@
+// Product class representing individual products
+class Product {
+    private String id;
+    private String name;
+    private String description;
+    private double price;
+
+    public Product(String id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    // Getter methods
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
